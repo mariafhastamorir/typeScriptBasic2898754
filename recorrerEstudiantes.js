73b8ceb6f1003f2c0cfa9e55c60b2099d384af87
mariafhastamorir <160276170+mariafhastamorir@users.noreply.github.com>
2024-10-09 19:22:01 -0500

    Delete recorrerEstudiantes.js

--- recorrerEstudiantes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const recorrerEstudiantes = (arregloEstudiantes) => {
-    arregloEstudiantes.forEach(function (estudiante) {
-        console.log(estudiante);
-        console.log("-----------");
-    });
-};
-exports.default = recorrerEstudiantes;
